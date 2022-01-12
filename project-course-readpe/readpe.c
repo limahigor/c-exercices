4530385bc7b33e9d5beafdb1f36c5e41fd9da546
Higor Lima <limahigor.g@gmail.com>
2022-01-11 21:25:52 -0300

    created ispe function

--- project-course-readpe/readpe.c
+++ project-course-readpe/readpe.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void fatal(char *msg){
 	fprintf(stderr, "Erro: %s\n", msg);
@@ -9,6 +10,10 @@ void fatal(char *msg){
 void usage(){
 	printf("Uso: readpe [arquivo]\nreadpe --help para mais informacoes\n");
 	exit(1);
+}
+
+bool ispe(const unsigned char *b){
+	return(b[0] != 'M' || b[1] != 'Z' || b[2] != 'P');
 }
 
 int main(int argc, char *argv[]){
@@ -26,9 +31,6 @@ int main(int argc, char *argv[]){
 
 	if(fread(buffer, 32, 1, fh) != 1)
 		fatal("nao consegui ler os 32 bytes arquivo");
-
-	if(buffer[0] != 'M' || buffer[1] != 'Z' || buffer[2] != 'P')
-		fatal("o arquivo nao parece ser um executavel");
 
 	return 0;
 }
