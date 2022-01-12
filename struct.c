36cfe12d6f50a5d5dda76421bc87b5f50edbdbc3
Higor Lima <limahigor.g@gmail.com>
2022-01-11 21:05:59 -0300

    create projectpe

--- struct.c
+++ struct.c
@@ -3,7 +3,18 @@
 #include <string.h>
 #define TAM 15
 
+struct st{
+	unsigned char id;
+	unsigned int num;
+};
+
 int main(int arc, char *argv[]){
+	struct st s;
+
+	s.id = 3;
+	s.num = 10;
+
+	printf("%zu\n", sizeof(struct st));
 	
 	return 0;
 }
